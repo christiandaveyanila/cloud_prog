327b02a60b81b2da1755da04f0800137b7338c9b
Christian Dave Yanila <128729596+christiandaveyanila@users.noreply.github.com>
2025-02-10 13:49:06 +0800

    Update HelloGitHub.java

--- HelloGitHub.java
+++ HelloGitHub.java
@@ -1,14 +1,13 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
+import java.util.Scanner;
 
 public class HelloGitHub
 {
-	public static void main(String[] args) {
-		System.out.println("Hello Github!");
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        
+        System.out.println("Hello, " + name + "!");
+    }
 }
